56c85089d497d686a67860b07f7e6de07aa913df
kapilsony21 <kapilsony21@appzephyr.com>
2023-08-27 16:23:48 +0530

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run() {
+        $users = [
+            [
+                'name' => 'Kapil Soni',
+                'email' => 'kapilsony21@gmail.com',
+                'password' => Hash::make('admin@kapil123')
+            ],
+            [
+                'name' => 'Shivam Soni',
+                'email' => 'shivamsoni@gmail.com',
+                'password' => Hash::make('admin@shivu123')
+            ]
+        ];
+
+        foreach ($users as $userData) {
+            \App\Models\User::create($userData);
+        }
+    }
+}
